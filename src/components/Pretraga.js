4520d0f9e58140302979cf3c33fb9ffa6a49ca6c
anchibela <anamaria.munjakovic@gmail.com>
2023-06-07 18:54:29 +0200

    dodan bootstrap i komponenta

---
+++ src/components/Pretraga.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export class Pretraga extends Component {
+  render() {
+    return (
+      <div className="pretraga">
+        <header className="App-header">
+          Unesite korisničko ime Github korisnika čije repozitorije želite
+          pregledati!
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Pretraga;
