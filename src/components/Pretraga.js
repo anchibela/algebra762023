aa6019628e4e3688b71a16e7bb951488581d79a6
anchibela <anamaria.munjakovic@gmail.com>
2023-06-07 19:06:42 +0200

    dodana forma

--- src/components/Pretraga.js
+++ src/components/Pretraga.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
+import Forma from "./Forma";
 
 export class Pretraga extends Component {
   render() {
     return (
       <div className="pretraga">
-        <header className="App-header">
-          Unesite korisničko ime Github korisnika čije repozitorije želite
-          pregledati!
-        </header>
+        <Forma></Forma>
       </div>
     );
   }
